fc07087e495b6d78b7cb1109cfbccbb85d2ffa77
Tobenna Ndu <tobennandu@gmail.com>
2015-12-04 21:12:40 +0100

    Added payment voucher auto-generation pathway

---
+++ all/themes/transparent/template.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Override the preprocess page on specific page views
+ * @param type $vars
+ */
+
+function transparent_preprocess_html (&$vars) {
+if (module_exists('path')) {
+    $alias = drupal_get_path_alias(str_replace('/edit','',$_GET['q']));
+    if ($alias != $_GET['q']) {
+      $template_filename = 'html';
+      foreach (explode('/', $alias) as $path_part) {
+        $template_filename = $template_filename . '__' . $path_part;
+        $vars['theme_hook_suggestions'][] = $template_filename;
+      }
+    }
+  }
+}
